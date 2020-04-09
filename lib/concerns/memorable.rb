3fcedc87d660e4331eea9d7c731d5b66c7e4e056
Brandon Guffey <hypefox@gmail.com>
2020-04-09 03:19:11 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -10,10 +10,13 @@ module Memorable
       self.all.count
     end
     
-    module InstanceMethods
+  end
+    
+  module InstanceMethods
       
-      def initialize
-        self.class.all << self
-      end
-      
+    def initialize
+      self.class.all << self
+    end
+    
+  end  
 end
